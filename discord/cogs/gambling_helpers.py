b3e23ecf3212af59f5386736e79b66c0fb2f4087
ashmourne2 <thienbrilliant12@gmail.com>
2026-02-05 16:38:57 +0700

    Refactor gambling_helpers.py for code consistency

--- discord/cogs/gambling_helpers.py
+++ discord/cogs/gambling_helpers.py
@@ -14,9 +14,9 @@ class GamblingHelpers(commands.Cog, name='General'):
     async def set(
         self,
         ctx: commands.Context,
-        user_id: int=None,
-        money: int=0,
-        credits: int=0
+        user_id: int = None,
+        money: int = 0,
+        credits: int = 0
     ):
         if money:
             self.economy.set_money(user_id, money)
@@ -24,21 +24,21 @@ class GamblingHelpers(commands.Cog, name='General'):
             self.economy.set_credits(user_id, credits)
 
     @commands.command(
-        brief=f"Gives you ${DEFAULT_BET*B_MULT} once every {B_COOLDOWN}hrs",
+        brief=f"Nhận ${DEFAULT_BET*B_MULT} mỗi {B_COOLDOWN} giờ",
         usage="add"
     )
-    @commands.cooldown(1, B_COOLDOWN*3600, type=commands.BucketType.user)
+    @commands.cooldown(1, B_COOLDOWN * 3600, type=commands.BucketType.user)
     async def add(self, ctx: commands.Context):
-        amount = DEFAULT_BET*B_MULT
+        amount = DEFAULT_BET * B_MULT
         self.economy.add_money(ctx.author.id, amount)
-        await ctx.send(f"Added ${amount} come back in {B_COOLDOWN}hrs")
+        await ctx.send(f"Đã cộng ${amount}, quay lại sau {B_COOLDOWN} giờ")
 
     @commands.command(
-        brief="How much money you or someone else has",
+        brief="Xem số tiền của bạn hoặc người khác",
         usage="money *[@member]",
         aliases=['credits']
     )
-    async def money(self, ctx: commands.Context, user: discord.Member=None):
+    async def money(self, ctx: commands.Context, user: discord.Member = None):
         user = user.id if user else ctx.author.id
         user = self.client.get_user(user)
         profile = self.economy.get_entry(user.id)
@@ -46,7 +46,7 @@ class GamblingHelpers(commands.Cog, name='General'):
             title=user.name,
             description=(
                 '**${:,}**'.format(profile[1]) +
-                '\n**{:,}** credits'.format(profile[2])
+                '\n**{:,}** tín dụng'.format(profile[2])
             ),
             footer=discord.Embed.Empty
         )
@@ -54,13 +54,13 @@ class GamblingHelpers(commands.Cog, name='General'):
         await ctx.send(embed=embed)
 
     @commands.command(
-        brief="Shows the user with the most money",
+        brief="Hiển thị người có nhiều tiền nhất",
         usage="leaderboard",
         aliases=["top"]
     )
     async def leaderboard(self, ctx):
         entries = self.economy.top_entries(5)
-        embed = make_embed(title='Leaderboard:', color=discord.Color.gold())
+        embed = make_embed(title='Bảng xếp hạng:', color=discord.Color.gold())
         for i, entry in enumerate(entries):
             embed.add_field(
                 name=f"{i+1}. {self.client.get_user(entry[0]).name}",
@@ -68,6 +68,7 @@ class GamblingHelpers(commands.Cog, name='General'):
                 inline=False
             )
         await ctx.send(embed=embed)
+
 
 def setup(client: commands.Bot):
     client.add_cog(GamblingHelpers(client))
